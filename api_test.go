be58dcffa91181e47776e56476860bb6ad2d6c82
QiFen.Pocket <QiFen.Pocket@gmail.com>
2021-01-04 02:18:05 +0800

    feat: add 60 seconds limit test

--- api_test.go
+++ api_test.go
@@ -28,6 +28,27 @@ func (t *ApiTestSuite) TearDownTest() {
 }
 
 func (t ApiTestSuite) TestReturnAccessCountByIp() {
+	jsonObject := GetResponse(t)
+
+	actual := int(jsonObject["count"].(float64))
+	expected := 1
+
+	t.Equal(expected, actual)
+}
+
+func (t ApiTestSuite) TestOnlyCanAccess60TimesPerMinute() {
+	for i := 0; i < 60; i++ {
+		GetResponse(t)
+	}
+	jsonObject := GetResponse(t)
+
+	actual := string(jsonObject["error"].([]byte))
+	expected := "Error"
+
+	t.Equal(expected, actual)
+}
+
+func GetResponse(t ApiTestSuite) map[string]interface{} {
 	response, _ := http.Get(t.TestServer.URL)
 
 	body, _ := ioutil.ReadAll(response.Body)
@@ -35,9 +56,5 @@ func (t ApiTestSuite) TestReturnAccessCountByIp() {
 
 	var jsonObject map[string]interface{}
 	json.Unmarshal(body, &jsonObject)
-
-	actual := int(jsonObject["count"].(float64))
-	expected := 1
-
-	t.Equal(expected, actual)
+	return jsonObject
 }
