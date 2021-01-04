4a4c6b8ba00b0719a9c2ef5c706d0a5b494e7f38
QiFen.Pocket <QiFen.Pocket@gmail.com>
2021-01-04 13:58:14 +0800

    feat: allow access test

--- rate_limiter_middleware.go
+++ rate_limiter_middleware.go
@@ -10,15 +10,16 @@ type AccessDenied struct {
 }
 
 type RateLimiterMiddleware struct {
-	accessCounter AccessCounter
+	AccessCounter AccessCounter
+	Handler       http.Handler
 }
 
-func NewRateLimiterMiddleware(accessCounter AccessCounter) *RateLimiterMiddleware {
-	return &RateLimiterMiddleware{accessCounter: accessCounter}
+func NewRateLimiterMiddleware(accessCounter AccessCounter, handler http.Handler) *RateLimiterMiddleware {
+	return &RateLimiterMiddleware{AccessCounter: accessCounter, Handler: handler}
 }
 
 func (r *RateLimiterMiddleware) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	count := r.accessCounter.Count(request.RemoteAddr, 60)
+	count := r.AccessCounter.Count(request.RemoteAddr, 60)
 
 	if count > 60 {
 		writer.Header().Set("Content-Type", "application/json")
--- rate_limiter_test.go
+++ rate_limiter_test.go
@@ -2,18 +2,27 @@ package RateLimiter
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 	"io/ioutil"
+	"net/http"
 	"net/http/httptest"
 	"testing"
 )
+
+type MockHandler struct {
+	mock.Mock
+}
+
+func (m *MockHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	m.Called(writer, request)
+}
 
 type RateLimiterMiddlewareSuite struct {
 	suite.Suite
 	sut *RateLimiterMiddleware
 	mockAccessCounter *MockAccessCounter
+	mockHandler *MockHandler
 }
 
 func TestRateLimiterMiddlewareSuiteInit(t *testing.T) {
@@ -21,14 +30,35 @@ func TestRateLimiterMiddlewareSuiteInit(t *testing.T) {
 }
 
 func (t *RateLimiterMiddlewareSuite) SetupTest() {
+	t.mockHandler = new(MockHandler)
 	t.mockAccessCounter = new(MockAccessCounter)
-	t.sut = NewRateLimiterMiddleware(t.mockAccessCounter)
+	t.sut = NewRateLimiterMiddleware(t.mockAccessCounter, t.mockHandler)
 }
 
 func (t RateLimiterMiddlewareSuite) TestDeniedAccess_After60TimesAccess_Within1Minute() {
 
-	t.mockAccessCounter.On("Count", mock.AnythingOfType("string"), 60).Return(61)
+	t.mockAccessCounter.On("Count", mock.Anything, 60).Return(61)
 
+	jsonObject := Get(&t)
+
+	actual := jsonObject["error"].(string)
+	expected := "Error"
+
+	t.Equal(expected, actual)
+}
+
+
+func (t RateLimiterMiddlewareSuite) TestAllowAccess() {
+
+	t.mockAccessCounter.On("Count", mock.Anything, 60).Return(1)
+	t.mockHandler.On("ServeHTTP", mock.Anything, mock.Anything).Return(nil)
+
+	Get(&t)
+
+	t.mockHandler.AssertCalled(t.T(), "ServeHTTP", mock.Anything, mock.Anything)
+}
+
+func Get(t *RateLimiterMiddlewareSuite) (jsonObject map[string]interface{}) {
 	writer := httptest.NewRecorder()
 	request := httptest.NewRequest("GET", "http://127.0.0.1", nil)
 
@@ -38,13 +68,6 @@ func (t RateLimiterMiddlewareSuite) TestDeniedAccess_After60TimesAccess_Within1M
 	body, _ := ioutil.ReadAll(response.Body)
 	response.Body.Close()
 
-	fmt.Println(string(body))
-
-	var jsonObject map[string]interface{}
 	json.Unmarshal(body, &jsonObject)
-
-	actual := jsonObject["error"].(string)
-	expected := "Error"
-
-	t.Equal(expected, actual)
+	return
 }
